819439b1db6e269754f6047f5a98431ead28e6b8
Jan Henckens <jan@studioespresso.co>
2017-06-16 21:43:38 +0200

    JS & CSS cleanup

--- unsplash/resources/js/Unsplash_Script.js
+++ unsplash/resources/js/Unsplash_Script.js
@@ -23,17 +23,11 @@ jQuery(document).ready(function($) {
                 dataType: 'JSON',
                 data: payload,
                 beforeSend: function() {
-                    console.log('Submitting image for download');
                     image.LoadingOverlay("show");
                 },
                 success: function(response) {
                     console.log(response);
                     image.LoadingOverlay("hide");
-                    var checkmark = '<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>';
-                    element.append(checkmark);
-                    setTimeout(function() {
-                        element.children('svg.checkmark').remove();
-                    }, 1400);
                 },
                 error: function(xhr, status, error) {
                     console.log(error);
